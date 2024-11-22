9c527c9ba97b33a06762c4c2fbc0cbbce95d5dd5
MUKESH474 <mmugesh0402@gmail.com>
2024-11-22 12:29:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='head'>
+        <lable htmlfor='term' >Guess the number from 1 to 10</lable> <br/>
+        <input type='text' name='text' id='term' />
+      </div>
     </div>
   );
 }
